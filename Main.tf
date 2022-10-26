eb6d35655de6b4f6881948632f175ef4ce272366
Niquan Lockhart <104804233+NiquanLockhart@users.noreply.github.com>
2022-10-26 14:01:27 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -11,6 +11,6 @@ resource "aws_budgets_budget" "monthly-budget" {
     threshold                  = 100
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
-    subscriber_email_addresses = ["user@example.com"]
+    subscriber_email_addresses = ["niquanlockhart@yahoo.com"]
   }
 }
